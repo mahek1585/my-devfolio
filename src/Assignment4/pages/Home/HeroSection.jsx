aff16038c1211f9938313fdd47f2f2d53b63e44c
Mahek <mahek1585@gmail.com>
2025-05-25 15:24:45 +0530

    changes done in Herosection

--- src/Assignment4/pages/Home/HeroSection.jsx
+++ src/Assignment4/pages/Home/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
     <div className="text-center md:text-left flex-1 max-w-[34rem] hero-content flex flex-col gap-5 md:gap-10">
         <p className="title text-xl font-semibold">Web Developer | Programmer </p>
         <h1 className="font-semibold text-[2.5rem] md:text-[5rem] leading-[100%] md:leading-[5rem]">Web Development Enthusiast</h1>
-        <p className="lead"> A passionate web developer building modern websites with React, JavaScript, and PHP. This portfolio showcases my latest work!</p>
+        <p className="lead"> A passionate web developer building modern websites with React and JavaScript. This portfolio highlights my latest work!</p>
     </div>
     <div className="hero-image">
         <img src={Heroimage} alt="Hero-image" className="w-[30rem]" />
